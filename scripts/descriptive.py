3df7834d627ee1bcb919ff1c72b294773350c769
Feysail Jemal <jemalfeysail@gmail.com>
2024-12-15 22:46:47 +0300

    final task-1

--- scripts/descriptive.py
+++ scripts/descriptive.py
@@ -5,7 +5,7 @@ def length(dataframe,column):
     return len(dataframe[column])
 
 def groupby(data_frame,group_column,count_column):
-    return data_frame.groupby(group_column)[[count_column]].count()
+    return data_frame.groupby(group_column)[[count_column]].count().sort_values(ascending=False)
 
 
 def change(data_frame,date):
--- scripts/plot.py
+++ scripts/plot.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def plot(dataframe,date,news):
     
     return plt.plot(dataframe.groupby(dataframe[date].dt.date)[news].count()),plt.ylabel('news')
+
+def sentiment(dataframe,column):
+   if column in dataframe.columns:
+        sns.histplot(data=dataframe, x=column, kde=True)
+        plt.title('Sentiment Distribution')
+        plt.xlabel('Sentiment')
+        plt.ylabel('Count')
+        plt.show()
+    
+   else: 
+     print(f"Column '{column}' does not exist in the DataFrame.")
+
